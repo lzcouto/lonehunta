a56fa0b7182d3b770b044795b83650ae95d34003
Lorenzo <lorenzo.couto@hotmail.com>
2014-02-11 15:50:46 -0200

    - Added 'Win' Condition - Basic Survivor Mode - 'Score' - Enemies Kill Functions - Do not go under -20.

--- public_html/js/lonehunta.js
+++ public_html/js/lonehunta.js
@@ -4,42 +4,61 @@ function preload() {
     game.load.image('ground', 'assets/platform.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     game.load.image('background', 'assets/platformer.png');
-    game.load.image('ground1', 'assets/ground.png');
     game.load.image('ground2', 'assets/ground2.png');
     game.load.image('enemy', 'assets/ufo.png');
+    game.load.spritesheet('trophy', 'assets/coin.png',32, 32);
 }
 
 var player;
+var trophy;
 var platforms;
 var cursors;
 var background;
 var enemy;
 var text;
 var dead;
+var winCount = 0;
+var score = '';
+var hard = 3;
 
 function create() {
     createBackground();
     createStage();
+    createTrophy();
     createPlayer();
+    createScore();
     enemy = game.add.group();
-    createEnemies(3);
+    createEnemies(hard);
     createControls();
-    createCamera();
+    createCamera();   
 }
 /* Create Functions */
 
+function createTrophy(){
+    trophy = game.add.sprite(2990, game.world.height - 150, 'trophy');
+    trophy.body.gravity.y = 40;
+    trophy.body.collideWorldBounds = true;
+    trophy.animations.add('spin', [0, 1, 2, 3, 4, 5], 10, true);
+    trophy.animations.play('spin');
+}
+
+function createScore(){
+    var style = { font: "bold 20pt Arial", fill: "#ffffff", align: "center", stroke: "#000000", strokeThickness: 2 };
+    score = game.add.text(game.camera.x + 10 , 10, 'W/D: ' + winCount, style);
+}
+
 function createEnemies(number){
     for(var i= 0; i < number; i++){  
-    enemies = enemy.create(Math.random() * 2000, 0,'enemy');
+    enemies = enemy.create(Math.random() * 3000, 0,'enemy');
     enemies.body.gravity.y = Math.random() * 10;
     enemies.body.bounce.y = 1.0;
     enemies.body.collideWorldBounds = true;
-     var tween = game.add.tween(enemies).to({ x: Math.random() * 2000 }, 2000, Phaser.Easing.Linear.None)
-    .to({ x: Math.random() * 2000 }, 1000, Phaser.Easing.Linear.None)
-    .to({ x: Math.random() * 2000 }, 2000, Phaser.Easing.Linear.None)
-    .to({ x: Math.random() * 2000 }, 1000, Phaser.Easing.Linear.None)
-    .to({ x: Math.random() * 2000 }, 2000, Phaser.Easing.Linear.None)
-    .to({ x: Math.random() * 2000 }, 1000, Phaser.Easing.Linear.None)
+    var tween = game.add.tween(enemies).to({ x: Math.random() * 3000 }, 2000, Phaser.Easing.Linear.None)
+    .to({ x: Math.random() * 3000 }, 1000, Phaser.Easing.Linear.None)
+    .to({ x: Math.random() * 3000 }, 2000, Phaser.Easing.Linear.None)
+    .to({ x: Math.random() * 3000 }, 1000, Phaser.Easing.Linear.None)
+    .to({ x: Math.random() * 3000 }, 2000, Phaser.Easing.Linear.None)
+    .to({ x: Math.random() * 3000 }, 1000, Phaser.Easing.Linear.None)
     .loop()
     .start();
     }
@@ -65,17 +84,17 @@ function createPlayer(){
 }
 
 function createStage(){
-    game.world.setBounds(0,0, 2000, 400);
+    game.world.setBounds(0,0, 3000, 400);
     platforms = game.add.group();
     var ground = platforms.create(0, game.world.height - 32, 'ground');
     ground.scale.setTo(20, 1);
     ground.body.immovable = true;
-    createRandomPlatforms(platforms, 10);
+    createRandomPlatforms(platforms, 20);
 }
 
 function createRandomPlatforms(group, number){
     for(var i = 0; i < number; i++){
-        var ledge = group.create(Math.random() * 2000, Math.random() * 350, 'ground2');
+        var ledge = group.create(Math.random() * 3000, Math.random() * 350, 'ground2');
         ledge.body.immovable = true;
         ledge.scale.setTo(0.2,0.2);
     }
@@ -88,33 +107,78 @@ function createControls(){
 function update() {
     collision();
     playerMovement();
-    
+    checkPlayerDeath();
+    updateScore();
+    checkImpossible();
+}
+
+/*Update Functions*/
+
+function checkPlayerDeath(){
     if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && dead)
     {
         revivePlayer();
     }
 }
 
-/*Update Functions*/
 function collision(){
     game.physics.collide(player, platforms);
     game.physics.collide(enemy, platforms);
-    game.physics.collide(player,enemy, killPlayer, null, this);
-    game.physics.overlap(player,enemy, killPlayer, null, this);
+    game.physics.collide(trophy,platforms);
+    game.physics.collide(player,enemy,killPlayer, null, this);
+    game.physics.overlap(player,enemy,killPlayer, null, this);
+    game.physics.collide(player,trophy,winGame,null,this);
+    game.physics.overlap(player,trophy,winGame, null, this);
 }
 
 function revivePlayer(){
     createPlayer();
     createCamera();
     text.destroy();
+    createEnemies(1);
     dead = false;
+}
+
+function checkImpossible(){
+    if(winCount < -20){
+        text.destroy();
+        var style = { font: "bold 20pt Arial", fill: "#ffffff", align: "center", stroke: "#000000", strokeThickness: 2 };
+        text = game.add.text(game.camera.x + 300, 150, 'NOOB - GameReset!', style);
+        killAllEnemies();
+        winCount = 0;
+    }
+}
+
+function winGame(player, trophy){
+    player.kill();
+    var style = { font: "bold 20pt Arial", fill: "#ffffff", align: "center", stroke: "#000000", strokeThickness: 2 };
+    text = game.add.text(game.camera.x + 170, 150, 'YOU WIN! (nothing) - Press Spacebar', style);
+    dead = true;
+    killEnemies();
+    winCount++;
 }
 
 function killPlayer(player, enemy){
     player.kill();
     var style = { font: "bold 20pt Arial", fill: "#ffffff", align: "center", stroke: "#000000", strokeThickness: 2 };
-    text = game.add.text(player.x - 200 ,100, 'GAMEOVER - Press Spacebar',style );
+    text = game.add.text(game.camera.x + 220 ,150, 'GAMEOVER - Press Spacebar',style );
     dead = true;
+    winCount--;
+}
+
+function killEnemies(){
+    enemy.getRandom().kill();
+}
+
+function killAllEnemies(){
+    enemy.callAll('kill');
+    createEnemies(hard);
+}
+
+function updateScore(){
+     score.destroy();
+     var style = { font: "bold 20pt Arial", fill: "#ffffff", align: "center", stroke: "#000000", strokeThickness: 2 };
+     score = game.add.text(game.camera.x + 10 , 10, 'W/D: ' + winCount, style);
 }
 
 function playerMovement(){
